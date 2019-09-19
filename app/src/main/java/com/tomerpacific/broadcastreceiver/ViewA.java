e6be036d9ddc74bbf747b08513b8cefc93287347
tmastrom <thomas.mastromonaco@gmail.com>
2019-09-19 15:49:33 -0400

    add log statements for debugging

--- app/src/main/java/com/tomerpacific/broadcastreceiver/ViewA.java
+++ app/src/main/java/com/tomerpacific/broadcastreceiver/ViewA.java
@@ -2,12 +2,14 @@ package com.tomerpacific.broadcastreceiver;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class ViewA extends MainActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.viewa);
+        Log.i("VIEW A", "View A created" );
     }
 }
 
